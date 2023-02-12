783a4bdaa966456f01c7921cd59d8132ce7847c6
itsmo1031 <itsmo1031@gmail.com>
2023-02-12 18:40:08 +0900

    🩹 fix: remove withContext phrase

--- src/test/kotlin/dev/itsmo/springintroduction/service/MemberServiceIntegrationTest.kt
+++ src/test/kotlin/dev/itsmo/springintroduction/service/MemberServiceIntegrationTest.kt
@@ -6,11 +6,8 @@ import io.kotest.core.extensions.Extension
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.extensions.spring.SpringExtension
 import io.kotest.matchers.shouldBe
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.annotation.Commit
 import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
@@ -26,12 +23,11 @@ class MemberServiceIntegrationTest(
             context("멤버를 주면") {
                 it("회원가입이 완료된다") {
                     val member = Member(name = "Spring")
-                    withContext(Dispatchers.IO) {
-                        val saveId = memberService.join(member)
-                        val findMember = memberService.findOne(saveId)
-                        
-                        member.name shouldBe findMember?.name
-                    }
+                    val saveId = memberService.join(member)
+                    val findMember = memberService.findOne(saveId)
+
+                    member.name shouldBe findMember?.name
+
                 }
             }
 
@@ -39,11 +35,9 @@ class MemberServiceIntegrationTest(
                 it("회원가입이 거절된다") {
                     val member1 = Member(name = "spring")
                     val member2 = Member(name = "spring")
-                    withContext(Dispatchers.IO) {
-                        memberService.join(member1)
-                        val e = shouldThrow<IllegalStateException> { memberService.join(member2) }
-                        e.message shouldBe "이미 존재하는 회원입니다."
-                    }
+                    memberService.join(member1)
+                    val e = shouldThrow<IllegalStateException> { memberService.join(member2) }
+                    e.message shouldBe "이미 존재하는 회원입니다."
                 }
             }
         }
